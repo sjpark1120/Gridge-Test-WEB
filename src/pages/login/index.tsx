1cc1a155c823d6d88933d083b4e0dac5c3922243
sujinpark <asgol1120@naver.com>
2024-03-10 01:53:31 +0900

    feat: /login 페이지 구현

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { InputWrap, LoginButton, LoginRoot, Wrap } from "./styles";
+import {
+  ErrorMessage,
+  KaKaoButton,
+  LoginBox1,
+  LoginBox2,
+  LoginBox3,
+  LoginButton,
+  LoginInput,
+  LoginRoot,
+  LogoImg,
+  MainImg,
+  Or,
+  PasswordBox,
+  PasswordToggle,
+  Signup,
+  StoreContainer,
+  Text1,
+  Text2,
+  Text3,
+} from "./styles";
 import { useRecoilState } from "recoil";
 import { jwtState, nameState } from "../../recoil/login";
+import kakaoIcon from "../../assets/kakao.png";
+import mailIcon from "../../assets/mail.png";
+import lockIcon from "../../assets/lock.png";
+import googlestoreImg from "../../assets/Mobile app store badge.png";
+import applestoreImg from "../../assets/Mobile app store badge apple.png";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
   const [name, setName] = useRecoilState(nameState);
+  const [password, setPassword] = useState("");
   const [, setJwt] = useRecoilState(jwtState);
+  const [errorText] = useState("");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isPasswordToggleVisible, setIsPasswordToggleVisible] = useState(false);
+  const [isLoginValid, setIsLoginValid] = useState(false);
 
-  // Input 에서 엔터키 누를 경우
+  // 비밀번호 에서 엔터키 누를 경우
   const onKeyUp = (event: React.KeyboardEvent<HTMLElement>) => {
     const key = event.key || event.keyCode;
     if (key === "Enter" || key === 13) {
@@ -20,6 +50,9 @@ const Login = () => {
   // 로그인 버튼 클릭
   const handleLogin = async () => {
     try {
+      if (!isLoginValid) {
+        return; // 비활성화 상태일 때는 클릭 이벤트를 처리하지 않음
+      }
       if (Object.keys(name).length === 0) {
         alert("이름을 입력해 주세요.");
         return;
@@ -35,35 +68,83 @@ const Login = () => {
     }
   };
 
+  const togglePasswordVisible = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
+  useEffect(() => {
+    //로그인 버튼 활성화 체크
+    if (name.length > 0 && password.length > 5) {
+      setIsLoginValid(true);
+    } else {
+      setIsLoginValid(false);
+    }
+  }, [name, password]);
+
+  useEffect(() => {
+    //비밀번호 토글 보이기 안보이기
+    if (password.length > 0) {
+      setIsPasswordToggleVisible(true);
+    } else {
+      setIsPasswordToggleVisible(false);
+    }
+  }, [password]);
+
   return (
     <LoginRoot>
-      <Wrap>
-        <div
-          style={{
-            fontSize: "2.5rem",
-            fontWeight: "600",
-            marginBottom: "4rem",
-          }}
-        >
-          로그인
-        </div>
-        <div
-          style={{
-            fontSize: "1.8rem",
-            fontWeight: "600",
-            marginBottom: "1rem",
-          }}
-        >
-          이름
-        </div>
-        <InputWrap
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="이름을 입력해주세요."
-          onKeyUp={onKeyUp}
-        />
-        <LoginButton onClick={handleLogin}>로그인</LoginButton>
-      </Wrap>
+      <MainImg />
+      <div>
+        <LoginBox1>
+          <LogoImg />
+          <LoginInput
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+            icon={mailIcon}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            maxLength={20}
+          />
+          <PasswordBox>
+            {isPasswordToggleVisible ? (
+              <PasswordToggle onClick={togglePasswordVisible}>
+                {isPasswordVisible ? "숨기기" : "비밀번호 표시"}
+              </PasswordToggle>
+            ) : null}
+            <LoginInput
+              placeholder="비밀번호"
+              icon={lockIcon}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type={isPasswordVisible ? "text" : "password"}
+              onKeyUp={onKeyUp}
+              maxLength={20}
+            />
+          </PasswordBox>
+          <LoginButton onClick={handleLogin} disabled={!isLoginValid}>
+            로그인
+          </LoginButton>
+          <Or>or</Or>
+          <KaKaoButton>
+            <img src={kakaoIcon} width="22px" />
+            카카오 로그인
+          </KaKaoButton>
+          <ErrorMessage>{errorText}</ErrorMessage>
+          <Text1>비밀번호를 잊으셨나요?</Text1>
+        </LoginBox1>
+        <LoginBox2>
+          <Text2>계정이 없으신가요?</Text2>
+          <Link to="/sign-up" style={{ textDecoration: "none" }}>
+            <Signup>가입하기</Signup>
+          </Link>
+        </LoginBox2>
+        <LoginBox3>
+          <Text3>앱을 다운로드 하세요</Text3>
+          <StoreContainer>
+            <img src={applestoreImg} width="120px" />
+            <img src={googlestoreImg} width="135px" />
+          </StoreContainer>
+        </LoginBox3>
+      </div>
     </LoginRoot>
   );
 };
--- src/pages/login/styles.tsx
+++ src/pages/login/styles.tsx
@@ -1,51 +1,148 @@
 import styled from "styled-components";
 import { supportDeviceSize } from "../../components/styles";
-
+import mockupImg from "../../assets/home-mockup.png";
+import logoImg from "../../assets/logo2.png";
 export const LoginRoot = styled.div`
   display: flex;
-  flex-direction: column;
+  /* flex-direction: column; */
   align-items: center;
   justify-content: center;
   width: 1080px;
   height: 100vh;
-  background-color: green;
+  /* background-color: green; */
 
   @media all and (max-width: ${supportDeviceSize}px) {
     width: 100vw;
   }
 `;
 
-export const Wrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: start;
-  justify-content: center;
-  width: 45rem;
-  height: 30rem;
-  padding: 3rem;
-  border: 0.5rem solid yellow;
-  background-color: red;
+export const MainImg = styled.div`
+  width: 520px;
+  height: 780px;
+  background-size: cover;
+  background-image: url(${mockupImg});
+  margin-right: 72px;
 `;
-
-export const InputWrap = styled.input`
+export const LoginBox1 = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 4rem;
-  padding-left: 1rem;
-  margin-bottom: 6rem;
-`;
+  width: 416px;
 
-export const LoginButton = styled.div`
-  width: 100%;
-  padding: 1rem;
-  border-radius: 0.6rem;
-  background-color: rgb(59, 105, 246);
-  font-size: 1.8rem;
-  font-weight: 600;
-  color: white;
+  background-color: white;
+  border: 1px solid #cccccc;
+  margin-bottom: 10px;
+`;
+export const LoginBox2 = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 416px;
+  height: 86px;
+  background-color: white;
+  border: 1px solid #cccccc;
+  margin-bottom: 20px;
+`;
+export const LoginBox3 = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 416px;
+`;
+export const LogoImg = styled.div`
+  width: 217px;
+  height: 80px;
+  background-size: cover;
+  background-image: url(${logoImg});
+  margin-top: 70px;
+  margin-bottom: 57px;
+`;
+export const LoginInput = styled.input<{ icon?: string }>`
+  width: 320px;
+  height: 50px;
+  border-radius: 30px;
+  border: 1px solid #b2b2b2;
+  padding: 10px 14px 10px 48px;
+  background: url(${(props) => props.icon}) no-repeat left 20px center;
+  background-size: 20px;
+  margin-bottom: 10px;
+`;
+export const LoginButton = styled.div<{ disabled: boolean }>`
+  width: 320px;
+  height: 44px;
+  border-radius: 30px;
+  background-color: ${(props) => (props.disabled ? "#b2ddff" : "#2E90FA")};
+  cursor: ${(props) => (props.disabled ? "default" : "pointer")};
   text-align: center;
+  margin-top: 10px;
+  color: #ffffff;
+  font-size: 16px;
+  line-height: 44px;
+  font-weight: 600;
+`;
+export const KaKaoButton = styled.div`
+  display: flex;
+  width: 320px;
+  height: 44px;
+  border-radius: 30px;
+  background-color: #f8d706;
+  margin-bottom: 30px;
+  color: #2f1b1a;
+  font-size: 16px;
+  line-height: 44px;
+  font-weight: 600;
+  align-items: center;
+  justify-content: center;
+`;
+export const Or = styled.div`
+  width: 14px;
+  height: 24px;
+  font-weight: 400;
+  font-size: 16px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+export const StoreContainer = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+export const Text1 = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  color: #7f7f7f;
+  margin-bottom: 40px;
+`;
+export const Text2 = styled.div`
+  font-size: 16px;
+  font-weight: 600;
+  color: #7f7f7f;
+`;
+export const Text3 = styled.div`
+  margin-bottom: 10px;
+  font-size: 16px;
+  font-weight: 500;
+  color: #7f7f7f;
+`;
+export const Signup = styled.div`
+  font-size: 16px;
+  font-weight: 600;
+  color: #2e90fa;
   cursor: pointer;
+`;
+export const ErrorMessage = styled.div`
+  font-size: 14px;
+  font-weight: 600;
+  color: #f04438;
+  margin-bottom: 10px;
+`;
+export const PasswordToggle = styled.div`
+  font-size: 16px;
+  font-weight: 600;
+  right: 20px;
+  top: 13px;
+  position: absolute;
+`;
+export const PasswordBox = styled.div`
+  position: relative;
 `;
